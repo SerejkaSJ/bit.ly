a701a0da5c37f9ed41e58b8b6b50c71ebd24c60b
SERGEYZHESTKOV\Sergey <serezha.zhestkov95@gmail.com>
2019-04-07 19:00:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,35 +4,34 @@ import json
 import os
 
 from dotenv import load_dotenv
-load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
 
 def create_short_link(token, link):
-  headers = {'Authorization': token}
-  payload = {
-		"long_url": link,
-	}
-  response = requests.post('https://api-ssl.bitly.com/v4/bitlinks', headers = headers, json=payload)
-  return json.loads(response.text)['link']
+    headers = {'Authorization': token}
+    payload = {
+        "long_url": link,
+    }
+    response = requests.post('https://api-ssl.bitly.com/v4/bitlinks', headers = headers, json=payload)
+    return json.loads(response.text)['link']
 
 
 def get_summary_link(token, link):
-  headers = {'Authorization': token}
-  response = requests.get('https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary'.format(link), headers = headers)
-  return json.loads(response.text)['total_clicks']
-  
-	
+    headers = {'Authorization': token}
+    response = requests.get('https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary'.format(link), headers = headers)
+    return json.loads(response.text)['total_clicks']
+
+
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser()
-	parser.add_argument("link")
-	args = parser.parse_args()
-	your_link = args.link
-	#your_link = input("Введите вашу ссылку ")
-	try:
-		print(get_summary_link(TOKEN, your_link))
-	except json.decoder.JSONDecodeError:
-		your_link = 'https://' + your_link
-		print(create_short_link(TOKEN, your_link))
-	except KeyError:
-		print(create_short_link(TOKEN, your_link))
+    load_dotenv()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("link")
+    args = parser.parse_args()
+    your_link = args.link
+    try:
+        print(get_summary_link(TOKEN, your_link))
+    except json.decoder.JSONDecodeError:
+        your_link = 'https://' + your_link
+        print(create_short_link(TOKEN, your_link))
+    except KeyError:
+        print(create_short_link(TOKEN, your_link))
